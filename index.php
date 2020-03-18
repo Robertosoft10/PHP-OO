7a03f2eca852d636106e5d097bcc42a66769b34f
José Roberto <roberto_silva-@hotmail.com>
2020-03-18 15:01:53 -0300

    Alterei a inde.php e o painel adm

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
                                     <input class="form-control" placeholder="Senha" name="password" type="password">
                                 </div>
                                 <!-- Change this to a button or input when using this as a form -->
-                                <button class="btn btn-success btn-block"><i class="fa fa-sign-in"></i> Login</button>
+                                <button class="btn btn-success btn-block"><i class="fa fa-sign-in"></i> Entrar</button>
                             </fieldset>
                         </form>
                     </div>
